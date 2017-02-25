0462542bba337ab2ca07048178f0b1810d727511
talta <t.taltallc@gmail.com>
2017-02-25 17:39:21 -0500

    test structure for the project

--- server.js
+++ server.js
@@ -1,5 +1,46 @@
+
+
+
  const express  = require('express');
  const app = express();
 
  app.use(express.static('public'));
  app.listen(process.env.PORT || 8080);
+
+
+
+ let server;
+
+ function runServer(){
+ 	const port = process.env.PORT || 8080;
+ 	return new Promise((resolve, reject) => {
+ 		server = app.listen(port, () =>{
+ 			console.log(`your app is listening on port ${port}`)
+ 			resolve(server);
+ 		});
+ 		.on('error', err=>{
+ 			resolve(err);
+ 		});
+ 	});
+ }
+
+function closeServer(){
+	return new Promise((esolve, reject){
+		console.log('closing server');
+		server.close(err =>{
+			if(err){
+				reject(err);
+				return;
+			}
+			resolve();
+		});
+	});
+}
+
+
+
+ if(require.main === module){
+ 	runServer().catch(err => console.error(err));
+ }
+
+ module.exports= {app, runServer, closeServer};

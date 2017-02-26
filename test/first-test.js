af272a8944e08a6493fadc625506545b00388362
talta <t.taltallc@gmail.com>
2017-02-26 09:38:06 -0500

    tests for the get call

--- test/first-test.js
+++ test/first-test.js
@@ -21,7 +21,24 @@ describe('exists', function(){
 		});
 
 	});
+});
 
+describe('locations severed', function(){
+	it('should get and display locations to the html', function(done){
+		chai.request(app)
+		.get('/')
+		.end(function(err, res){
+			res.should.have.status(200);
+			res.should.be.html;
+			res.body.length.should.be.at.least(1);
+			res.body.should.be.a('object');
+			const expectedKeys = ["city-country", "latitude", "longitude", "notes"];
+			res.body.forEach(function(item){
+				item.should.be.a('object');
+				item.should.include.keys(expectedKeys);
+			});
+		});
+	});
 });
 
 

02b4d48c19796b92c0480b5651cf15c5f518db32
talta <t.taltallc@gmail.com>
2017-02-26 05:39:57 -0500

    added mock data for the locations

---
+++ public/client-mock.js
@@ -0,0 +1,34 @@
+const Mock_Locations_Data ={
+	"locations" = [
+		{
+			"city-country": "Rome, Italy",
+			"latitude":"41.9028",
+			"longitude": "12.4964",
+			"notes":"I have been here.  great city.  I loved the history, but the ruins were disappointing"
+		},
+				{
+			"city-country": "Bangkok, Thailand",
+			"latitude":"13.7563",
+			"longitude": "100.5018",
+			"notes":"ancient city of the capitol of Siam, one of the most important trading cities in 1800's.  The King and I was based on this city."
+		},
+				{
+			"city-country": "Akron, Ohio",
+			"latitude":"41.0814",
+			"longitude": "81.5190",
+			"notes":""
+		},
+				{
+			"city-country": "Helsinki, Finland",
+			"latitude":"60.1699",
+			"longitude": "24.9384",
+			"notes":"I really want to go to grad school here.  Close to the fyords and denmark for access to the rest of europe"
+		},
+				{
+			"city-country": "Rome, Italy",
+			"latitude":"41.3851",
+			"longitude": "2.1734",
+			"notes":"I went here to study abroad in high school.  absolutely beautiful.  Fashion capital, good food and tons of culture."
+		}
+	]
+}

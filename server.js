4197d91cf819a1fc78a8da37ba13ba984e0c2782
talta <t.taltallc@gmail.com>
2017-02-26 21:23:59 -0500

    changes to the feature, just needs quality assured

--- server.js
+++ server.js
@@ -9,51 +9,51 @@
 
 
 
-//  let server;
+ let server;
 
-//  function runServer(){
-//  	const port = process.env.PORT || 8080;
-//  	return new Promise((resolve, reject) => {
-//  		server = app.listen(port, () =>{
-//  			console.log(`your app is listening on port ${port}`)
-//  			resolve();
-//  		});
-//  		.on('error', err => {
-//  			reject(err);
-//  		});
-//  	});
-//  }
+ function runServer(){
+ 	const port = process.env.PORT || 8080;
+ 	return new Promise((resolve, reject) => {
+ 		server = app.listen(port, () =>{
+ 			console.log(`your app is listening on port ${port}`)
+ 			resolve();
+ 		});
+ 		.on('error', err => {
+ 			reject(err);
+ 		});
+ 	});
+ }
 
-//   function runServer(){
-//  	const port = process.env.PORT || 8080;
-//  	return new Promise((resolve, reject) => {
-//  		server = app.listen(port, () =>{
-//  			console.log(`your app is listening on port ${port}`)
-//  			resolve(server);
-//  		});
-//  		.on('error', err => {
-//  			reject(err);
-//  		});
-//  	});
-//  }
+  function runServer(){
+ 	const port = process.env.PORT || 8080;
+ 	return new Promise((resolve, reject) => {
+ 		server = app.listen(port, () =>{
+ 			console.log(`your app is listening on port ${port}`)
+ 			resolve(server);
+ 		});
+ 		.on('error', err => {
+ 			reject(err);
+ 		});
+ 	});
+ }
 
-// function closeServer(){
-// 	return new Promise((esolve, reject){
-// 		console.log('closing server');
-// 		server.close(err =>{
-// 			if(err){
-// 				reject(err);
-// 				return;
-// 			}
-// 			resolve();
-// 		});
-// 	});
-// }
+function closeServer(){
+	return new Promise((esolve, reject){
+		console.log('closing server');
+		server.close(err =>{
+			if(err){
+				reject(err);
+				return;
+			}
+			resolve();
+		});
+	});
+}
 
 
 
- // if(require.main === module){
- // 	runServer().catch(err => console.error(err));
- // }
+ if(require.main === module){
+ 	runServer().catch(err => console.error(err));
+ }
 
  module.exports= {app};
